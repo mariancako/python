b622e6499df5011f0fa9c14e3f2ae63e2f7b674b
Marian <marian.cako@gmail.com>
2022-10-04 14:58:06 +0200

    change

--- 12.py
+++ 12.py
@@ -1,9 +1,9 @@
 elso=12
 masodik=24
 harmadik=34
-print('elso szam:12')
-print('masodik szam:24')
-print('harmadik szam:34')
+print('elso szam:', elso)
+print('masodik szam:', masodik)
+print('harmadik szam:', harmadik)
 
 print(elso+masodik+harmadik)
 print(elso*masodik)
